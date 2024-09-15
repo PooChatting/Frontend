0dff78aaf88c94b4568a1a19f006dbdc1dbb93cb
lasgra <tomekoclon1@gmail.com>
2024-09-15 19:22:19 +0200

    login SpinalCore

--- src/app/layouts/routes/routes.component.ts
+++ src/app/layouts/routes/routes.component.ts
@@ -1,10 +1,18 @@
 import {Routes} from "@angular/router";
+import { IsLoggedGuard } from "../../shared/utility/IsLoggedGuard";
+
 
 export const APP_LAYOUT_ROUTES: Routes = [
   {
     path: 'dm/:id',
+    canActivate: [IsLoggedGuard],
     loadComponent: () => import('../../views/direct-messages/direct-messages.component')
       .then(m => m.DirectMessagesComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('../../views/login/login.component')
+      .then(m => m.LoginComponent)
   },
 
 ]
---
+++ src/app/shared/dtos/RegisterDto.ts
@@ -0,0 +1,6 @@
+﻿export interface RegisterDto {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
