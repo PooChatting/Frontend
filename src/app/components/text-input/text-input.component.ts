41c7a77499926d96cd656706e863f247f8f7a852
lasgra <tomekoclon1@gmail.com>
2024-10-05 15:38:34 +0200

    changed text input

--- src/app/components/text-input/text-input.component.ts
+++ src/app/components/text-input/text-input.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, HostListener, inject, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Output, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -10,27 +10,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './text-input.component.scss'
 })
 export class TextInputComponent {
-  textInputHeight: number = 56
-  textareaValue: string = ""
+  @ViewChild('inputDiv') inputDiv!: ElementRef;
   @Output() sendMessage = new EventEmitter<string>();
   @Output() inputHeight = new EventEmitter<number>();
 
-  messageChange(event: Event){
-    let textArea = event.target as HTMLTextAreaElement
-    textArea.style.height = "40px"
-    textArea.style.height = textArea.scrollHeight + "px"
-    this.textInputHeight = (textArea.scrollHeight + 16)
-    this.inputHeight.emit(this.textInputHeight)
-    
+  messageChange(element: HTMLDivElement){
+    this.inputHeight.emit(parseInt(element.style.height.split("px")[0]))
   }
 
   @HostListener('document:keypress', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) { 
     if(!event.shiftKey && event.key == "Enter"){
-      this.sendMessage.emit(this.textareaValue)
-      this.textareaValue = ""
-      this.textInputHeight = 56
-      this.inputHeight.emit(this.textInputHeight)
+      this.sendMessage.emit(this.inputDiv.nativeElement.innerHTML)
+      this.inputDiv.nativeElement.innerHTML = ""
+      this.inputHeight.emit(56)
       event.preventDefault();
     }
   }
--- src/app/components/text-message/text-message.component.ts
+++ src/app/components/text-message/text-message.component.ts
@@ -1,15 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MessageDto } from '../../shared/dtos/MessageDto';
 import { IconButtonComponent } from "../icon-button/icon-button.component";
 import { CommonModule } from '@angular/common';
+import { TextInputComponent } from "../text-input/text-input.component";
 
 @Component({
   selector: 'app-text-message',
   standalone: true,
   templateUrl: './text-message.component.html',
-  imports: [IconButtonComponent, IconButtonComponent, CommonModule]
+  imports: [IconButtonComponent, IconButtonComponent, CommonModule, TextInputComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TextMessageComponent {
   @Input({ required: true }) messageDto!: MessageDto
   buttonsVisible: boolean = false
+  isEditting: boolean = false
+
+  editMessage(){
+    this.isEditting = !this.isEditting
+  }
 }
